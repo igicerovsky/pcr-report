bdb296dfad2cdc2ccd53eca7defe3107a66c28b9
Igor Cerovsky <igor.cerovsky@takeda.com>
2023-10-10 14:49:12 +0200

    checks

--- pcrep/check.py
+++ pcrep/check.py
@@ -6,55 +6,8 @@ MIN_3S_NAME = 'lower 3s action'
 MAX_3S_NAME = 'upper 3s action'
 MIN_2S_NAME = 'lower 3s warning'
 MAX_2S_NAME = 'upper 3s warning'
-MIN_METHOD_NAME = 'Lower [vg/ml]'
-MAX_METHOD_NAME = 'Upper [vg/ml]'
-
-
-def check_routing(limitsdc, type, val, target_id):
-    """Routing of checks
-
-    Parameters:
-    -----------
-    limitsdc: disctionary
-        Dictionary with limits
-    type: str
-        Sample type one of 'nc', 'rc', 's'
-    val: float
-        Value to check
-    target_id: str
-        Method target one of target_id from method_limits.csv
-    """
-    ret = None
-    if type == 'nc':
-        t = limitsdc['method'].loc[:, 'Lower [vg/ml]'][target_id] * \
-            METHOD_LIMIT_MULTIPLIER_NEGATIVE_CONTROL
-        ret = check_nc(t, val)
-    elif type == 'rc':
-        ret = check_control(
-            limitsdc['reference_control'].loc[target_id], val, type)
-    elif type == 'pc':
-        ret = check_control(limitsdc['plasmid_control'][target_id], val, type)
-    elif type == 's':
-        ret = None
-    else:
-        ret = check_s(limitsdc['method'].loc[target_id], val, type)
-        raise Exception(f'Invalid sample type {type} in check_routing!')
-
-    return ret
-
-
-def check_nc(thr, val):
-    """Check negative control
-
-    Parameters:
-    -----------
-    thr: float
-        Lower limit threshold
-    val: float
-        Value to check
-    """
-
-    return (val < thr, f'nc > threshold ({val} > {t})')
+MIN_METHOD_NAME = 'Lower [vg/μl]'
+MAX_METHOD_NAME = 'Upper [vg/μl]'
 
 
 def check_limits(min, max, val, type):
@@ -84,9 +37,93 @@ def check_control(limits, val, type):
     return r3s
 
 
-def check_s(limits, val, type):
+def control_check_routing(limitsdc, type, val, target_id):
+    """Routing of CONTROL checks
+
+    Parameters:
+    -----------
+    limitsdc: disctionary
+        Dictionary with limits
+    type: str
+        Sample type, valid for 'pc', 'rc' otherwise skipped
+    val: float
+        Value to check
+    target_id: str
+        Method target one of target_id from method_limits.csv
+    """
+    ret = None
+    if type == 'rc':
+        check_control(limitsdc['reference_control'][target_id], val, type)
+    elif type == 'pc':
+        check_control(limitsdc['plasmid_control'][target_id], val, type)
+
+    return ret
+
+
+def method_check_routing(limits, type, val, target_id):
+    """Routing of METHOD checks
+
+    Parameters:
+    -----------
+    limitsdc: disctionary
+        Dictionary with limits
+    type: str
+        Sample type one of 'nc', 'pc', 'rc', 's'
+    val: float
+        Value to check
+    target_id: str
+        Method target one of target_id from method_limits.csv
+    """
+    ret = None
+    if type == 'nc':
+        # order of magnitude lower than negative control limits
+        t = limits.loc[target_id][MIN_METHOD_NAME] * \
+            METHOD_LIMIT_MULTIPLIER_NEGATIVE_CONTROL
+        ret = method_check_nc(t, val)
+    elif type == 'rc' or type == 'pc' or type == 's':
+        ret = method_check_s(limits.loc[target_id], val, type)
+    else:
+        raise Exception(f'Invalid sample type {type} in check_routing!')
+
+    return ret
+
+
+def method_check_nc(thr, val):
+    """Check negative control
+
+    Parameters:
+    -----------
+    thr: float
+        Lower limit threshold
+    val: float
+        Value to check
+    """
+    comment = None
+    if val > thr:
+        comment = f'nc > threshold ({val} > {thr})'
+    return (comment == None, comment)
+
+
+def method_check_s(limits, val, type):
     """Check sample
     """
 
     return check_limits(limits[MIN_METHOD_NAME],
                         limits[MAX_METHOD_NAME], val, type)
+
+
+def droplets_check(droplets_num: int, low_thr: int):
+    """Check number of droplets
+
+    Parameters:
+    -----------
+    low_thr: int
+        Minumum droplets available
+    droplets_num: int
+        Actual number of droplets
+    """
+
+    comment = None
+    if droplets_num < low_thr:
+        comment = f'droplets < threshold ({droplets_num} < {low_thr})'
+    return (comment == None, comment)
