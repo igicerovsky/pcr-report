1464476e6ad760430c0a2091996964fd84e1dc90
Igor Cerovsky <igor.cerovsky@takeda.com>
2023-10-12 09:13:55 +0200

    wip checks

--- pcrep/check.py
+++ pcrep/check.py
@@ -1,6 +1,8 @@
 """Script containing method checks
 """
 
+from enum import Enum
+
 METHOD_LIMIT_MULTIPLIER_NEGATIVE_CONTROL = 0.1
 MIN_3S_NAME = 'lower 3s action'
 MAX_3S_NAME = 'upper 3s action'
@@ -8,6 +10,19 @@ MIN_2S_NAME = 'lower 3s warning'
 MAX_2S_NAME = 'upper 3s warning'
 MIN_METHOD_NAME = 'Lower [vg/μl]'
 MAX_METHOD_NAME = 'Upper [vg/μl]'
+CV_THRESHOLD = 20.0  # in %
+
+
+class CheckLevel(int, Enum):
+    WARNING = 1
+    ERROR = 0
+
+
+class CheckType(str, Enum):
+    METHOD = 'method'
+    NEGATIVE_CONTROL = 'negative control'
+    PLASMID_CONTROL = 'plasmid_control'
+    CV = 'CV'
 
 
 def check_limits(min, max, val, type):
@@ -16,7 +31,7 @@ def check_limits(min, max, val, type):
         comment = f'{type} {val} < {min}'
     elif val > max:
         comment = f'{type} {val} > {max}'
-    return (comment == None, comment)
+    return comment
 
 
 def check_control(limits, val, type):
@@ -53,9 +68,9 @@ def control_check_routing(limitsdc, type, val, target_id):
     """
     ret = None
     if type == 'rc':
-        check_control(limitsdc['reference_control'][target_id], val, type)
+        check_control(limitsdc['reference_control'].loc[target_id], val, type)
     elif type == 'pc':
-        check_control(limitsdc['plasmid_control'][target_id], val, type)
+        check_control(limitsdc['plasmid_control'].loc[target_id], val, type)
 
     return ret
 
@@ -101,7 +116,7 @@ def method_check_nc(thr, val):
     comment = None
     if val > thr:
         comment = f'nc > threshold ({val} > {thr})'
-    return (comment == None, comment)
+    return comment
 
 
 def method_check_s(limits, val, type):
@@ -126,4 +141,20 @@ def droplets_check(droplets_num: int, low_thr: int):
     comment = None
     if droplets_num < low_thr:
         comment = f'droplets < threshold ({droplets_num} < {low_thr})'
-    return (comment == None, comment)
+    return comment
+
+
+def cv_check(val, thr=CV_THRESHOLD):
+    """Coefficient of variation check
+
+    Parameters:
+    -----------
+    val: float
+        Value to check
+    thr: float
+        CV threshold in %
+    """
+    comment = None
+    if val > thr:
+        comment = f'CV > threshold ({val} > {thr})'
+    return comment
